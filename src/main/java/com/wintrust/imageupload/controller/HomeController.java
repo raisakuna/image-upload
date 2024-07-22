dc9fd781e61bd1f7aa5fbb1dd9f0f1ae660353a1
raisakuna <sakunaxl@gmail.com>
2024-07-22 15:31:13 -0400

    initial commit

---
+++ src/main/java/com/wintrust/imageupload/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.wintrust.imageupload.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Welcome to image uploading app in spring boot";
+    }
+}
